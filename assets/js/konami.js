25caad46e9962d6460dca4ab69413100fd5427ed
wonder-coconut <rrishab2001@gmail.com>
2021-04-05 22:08:30 +0530

    added credits

--- assets/js/konami.js
+++ assets/js/konami.js
@@ -1,3 +1,4 @@
+/*credit to user Ehsan Kia in the forum https://stackoverflow.com/questions/31626852/how-to-add-konami-code-in-a-website-based-on-html*/
 let cursor = 0;
 const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 document.addEventListener('keydown', (e) => {
